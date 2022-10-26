f2a89a05bba1dacadcefadc0e60c79786687e3cb
??Args?? <81772795+Pragrammist@users.noreply.github.com>
2022-10-26 14:46:28 +0300

    Update HIRALogin.cs

--- HendInRentApi/APIs/HIRALogin.cs
+++ HendInRentApi/APIs/HIRALogin.cs
@@ -1,6 +1,6 @@
 ﻿namespace HendInRentApi
 {
-    public interface HIRALogin<TLoginResult, TLoginInputData>
+    public interface HIRALogin<TLoginResult, TLoginInputData> // это интерфейс используется в WEB проекте
     {
         Task<TLoginResult> Login(TLoginInputData user);
     }
