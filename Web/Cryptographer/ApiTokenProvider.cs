5abf2be6530916cf580b5210c2b6de86c9a5c38c
??Args?? <81772795+Pragrammist@users.noreply.github.com>
2022-10-26 16:35:37 +0300

    Update ApiTokenProvider.cs

--- Web/Cryptographer/ApiTokenProvider.cs
+++ Web/Cryptographer/ApiTokenProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace Web.Cryptographer
 {
+    //todo move in another namespace and folder
     public interface ApiTokenProvider
     {
         public Task<string> GetToken(string password, string login);
