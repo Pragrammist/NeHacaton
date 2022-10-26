f83a1d2c4ea872a5477156b8fef4bac34594584b
??Args?? <81772795+Pragrammist@users.noreply.github.com>
2022-10-26 15:08:32 +0300

    Update SaleService.cs

--- Web/Services/SaleService.cs
+++ Web/Services/SaleService.cs
@@ -45,7 +45,9 @@ namespace Web.Services
             await foreach (var u in _userContext.Users)
             {
                 var inventories = await FilerByCityAndGetOutputInventories(u, input, HIRAInput);
-                foreach (var inventory in inventories)
+                foreach (var inventory in inventories) 
+                 // я бы с радостью как-то убрал бы этот цикл в метод
+                 // но для того, чтобы сделать вывод с помщью yield return, нужна эта вложенность
                 {
                     yield return inventory;
                 }
@@ -55,10 +57,11 @@ namespace Web.Services
 
 
         #region help methods for GetInventories
+        // метод просто переносит логику из цикла foreach в GetInventories,
         async Task<IEnumerable<OutputInventoryDto>> FilerByCityAndGetOutputInventories(User user, InputSearchInventoryDto? input, InputHIRAInventoryDto HIRAInput)
         {
             if (!await FilterByCity(user, input?.Lat, input?.Lon))
-                return new OutputInventoryDto[] { };
+                return new OutputInventoryDto[] { }; // выводиться пустой массив а не null, чтобы не сломать foreach
 
             var token = await GetToken(user);
             var res = await GetOutputInventories(input, HIRAInput, token);
@@ -67,15 +70,19 @@ namespace Web.Services
 
         async Task<IEnumerable<OutputInventoryDto>> GetOutputInventories(InputSearchInventoryDto? input, InputHIRAInventoryDto? HIRAInput, string token)
         {
-            var HIRAInventoriesResult = await _inventoryRepo.MakePostJsonTypeRequest(POST_INVENTORY_ITEMS, token, HIRAInput);
-            if (HIRAInventoriesResult.Array != null && HIRAInventoriesResult.Array.Count > 0)
+            var HIRAInventoriesResult = await _inventoryRepo.MakePostJsonTypeRequest(POST_INVENTORY_ITEMS, token, HIRAInput); // запрос и ответ от апи
+            if (HIRAInventoriesResult.Array != null && HIRAInventoriesResult.Array.Count > 0) // чтобы не передать пустой массив метод для поиска тегов
             {
                 var inventoriesResultDto = _mapper.Map<OutputInventoriesResultDto>(HIRAInventoriesResult);
                 return _searcher.SelectInventoriesByTags(input?.Tags, inventoriesResultDto.Array);  
             }
             return new OutputInventoryDto[] { };
         }
-        async Task<bool> FilterByCity(User user, double? lat, double? lon)
+        async Task<bool> FilterByCity(User user, double? lat, double? lon) 
+        // ну фильтр - громко сказано, просто дает true или fakse 
+        // при сравнивании города из user(это арендодатель) и координатов пользователя решивший посмотреть каталог
+        // TODO сделать запоминание города хранилище
+        // TODO переменовать класс User
         {
             if (lat == null || lon == null)
                 return true;
