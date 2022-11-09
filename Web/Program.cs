dfb285579d362d42b171263e88ad5ad4d4a53a40
Vitality Kovalenko <vitalcik.kovalenko2019@gmail.com>
2022-11-10 02:05:37 +0300

    конфиг корсаD

--- Web/Program.cs
+++ Web/Program.cs
@@ -49,7 +49,7 @@ namespace Web
 
             app.UseRouting();
 
-            app.UseCors(b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseCors(b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
             app.UseAuthentication();    
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
